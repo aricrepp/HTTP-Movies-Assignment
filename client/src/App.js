9dcc0503b8841a63c67d2f883e837dac157350bb
Aric Repp <aricmrepp@gmail.com>
2020-07-15 23:31:25 -0400

    Wrapping up mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import UpdateMovie from './Movies/UpdateMovie';
+import Movie from './Movies/Movie';
 import axios from 'axios';
 
 const App = () => {
@@ -11,12 +13,12 @@ const App = () => {
 
   const getMovieList = () => {
     axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
+      .get('http://localhost:5000/api/movies')
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.log(err.response));
   };
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
@@ -28,13 +30,30 @@ const App = () => {
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
-        <MovieList movies={movieList} />
-      </Route>
+      <Route
+        exact
+        path="/"
+        render={(props) => <MovieList {...props} movies={movieList} />}
+      />
 
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
-      </Route>
+      <Route
+        path="/movies/:id"
+        render={(props) => (
+          <Movie
+            {...props}
+            movieList={movieList}
+            setMovieList={setMovieList}
+            addToSavedList={addToSavedList}
+          />
+        )}
+      />
+
+      <Route
+        path="/update-movies/:id"
+        render={() => (
+          <UpdateMovie movieList={movieList} setMovieList={setMovieList} />
+        )}
+      />
     </>
   );
 };
